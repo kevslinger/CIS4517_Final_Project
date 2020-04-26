51a1103c0d654dd804cd85c310d70adb5d2d468e
kevslinger <tug74186@temple.edu>
2020-04-25 21:52:15 -0400

    added removal of retweets

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import argparse
 import glob
 import os
+import time
 
 # Do we want to remove all RTs?
 # Definitely want to filter by Tweet ID -- keep a set of unique Tweet IDs
@@ -22,6 +23,9 @@ import os
 # DO NOT DELETE OR REMOVE ANY DATA/ATTRIBUTES
 # STEP 3: Write a report (sample given in step 2), documenting each step you took to clean the data.
 
+# Read a directory for all CSV files, and concatenate them into one DataFrame
+# NOTE: Your computer might not be able to handle this!! That's why it's commented out in main.
+# If you dataset is very small, consdier commenting out the loop in main function and concatenating.
 def read_tweets(data_dir):
     file_list = glob.glob(os.path.join(data_dir, '*.csv'))
     df = pd.concat([pd.read_csv(file_list[i], low_memory=False) for i in range(len(file_list))], ignore_index=True)
@@ -33,19 +37,28 @@ def remove_duplicates(df):
     return df
 
 def remove_RT(df):
-    is_RT = df[df['full_text'][:2] == 'RT']
-
+    is_RT = df[df['full_text'].str.startswith("RT ")]
+    df.drop(is_RT.index, axis=0, inplace=True)
+    return df
 
     
 def main(args):
+    # If you have a small dataset, consider uncommenting these two lines
+    # If you do, you need to change the df.to_csv lines that use path.split.
     #df = read_tweets(args.data_dir)
-    #df.to_csv('/Users/kevin/Desktop/test_concat_df.csv', index=False)
+    #df.to_csv(os.path.join(args.output_dir, 'concatenated_df.csv'), index=False)
+    # Since our dataset is not very small, we will iteratively apply the cleaning process
+    # To each CSV file.
     for path in glob.glob(os.path.join(args.data_dir, '*.csv')):
+        # Read in one csv at a time and then remove duplicate Tweet IDs, then remove Retweets, and
+        # save the CSV at each step to record progress.
         df = pd.read_csv(path)
         print("Read in " + path.split('/')[-1])
         df = remove_duplicates(df)
-        df.to_csv(os.path.join(args.output_dir, path.split('/')[-1]), index=False)
+        df.to_csv(os.path.join(args.output_dir, "deduped_" + path.split('/')[-1]), index=False)
         print("Wrote " + path.split('/')[-1])
+        df = remove_RT(df)
+        df.to_csv(os.path.join(args.output_dir, "removed_RT_" + path.split('/')[-1], index=False))
         del df
 
 if __name__ == '__main__':
